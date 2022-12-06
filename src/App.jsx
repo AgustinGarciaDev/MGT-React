0d255de5a0cb8fc35a3c7e81e6d36f18cc95f55d
C06831 <66225450+AgustinGarciaDev@users.noreply.github.com>
2022-12-06 19:23:38 -0300

    Add page support

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Component/Header';
 import Home from './Component/Home';
+import Contact from './Component/Contact'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+   
+      <BrowserRouter>
+        <Header/>
+        <div className="App">
+        <Routes>
+          <Route exact path="/" element = {<Home/>} />
+          <Route exact path="/support" element = {<Contact/>} />
+        </Routes>
+        </div>
+       </BrowserRouter>
+
   )
 }
 
---
+++ src/Component/Contact.jsx
@@ -0,0 +1,13 @@
+const Contact = () => { 
+
+    return (
+        <div className="containerPrivacity">
+           <h1> Support </h1>
+           <p>
+           Hello, if you are having problems with our application. Write to this email 
+           infostudioapp@gmail.com
+           </p>
+        </div>
+    )
+}
+export default Contact
